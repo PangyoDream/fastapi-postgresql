5f132d5f0e511a5eded375540cb640a96f4f3e68
marudengi <dame95@naver.com>
2022-10-31 22:21:48 +0900

    Update sql_app blind unuse functions

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -2,15 +2,14 @@ from sqlalchemy.orm import Session
 
 from sql_app import models, schemas
 
+# def get_user(db: Session, user_id: int):
+#     return db.query(models.User).filter(models.User.id == user_id).first()
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
+# def get_user_by_name(db: Session, name: str):
+#     return db.query(models.User).filter(models.User.name == name).first()
 
-def get_user_by_name(db: Session, name: str):
-    return db.query(models.User).filter(models.User.name == name).first()
-
-def get_users(db: Session, skip: int = 0, limit: int = 100):
-    return db.query(models.User).offset(skip).limit(limit).all()
+# def get_users(db: Session, skip: int = 0, limit: int = 100):
+#     return db.query(models.User).offset(skip).limit(limit).all()
 
 def get_saju(db: Session, gender: str, year: int, month: int, day: int):
     return db.query(models.Saju).filter(models.Saju.gender == gender
